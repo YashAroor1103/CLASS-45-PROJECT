23bb522148746c6a3457d7cbca547eb81dd4aa0e
YashAroor1103 <74197430+YashAroor1103@users.noreply.github.com>
2021-07-17 17:14:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var gameState = 0;
+var playButton;
+var backGroundImage;
+var ball;
+var ballImage;
+var player;
+var playerImage;
+var opponent;
+var opponentImage;
+
+function preload(){
+  backGroundImage = loadImage("bg2.png");
+  ballImage = loadImage("ball.png");
+  playerImage = loadImage("boy.png");
+  opponentImage = loadImage("girl.png");
+}
+
+function setup() {
+  createCanvas(displayWidth , displayHeight);
+
+  ball = createSprite(displayWidth/2 , displayHeight/2 , 20 , 20);
+  ball.addImage("ballImage",ballImage);
+  ball.scale = 0.2;
+
+  player = createSprite(displayWidth/2, displayWidth/2.5 , 20 , 20);
+  player.addImage("playerImage",playerImage);
+  player.scale = 2;
+
+  opponent = createSprite(displayWidth/2 , displayHeight/6 , 20 , 20);
+  opponent.addImage("opponentImage",opponentImage);
+  opponent.scale = 2;
+
+  playButton = new Button();
+
+}
+
+function draw() {
+  background("red");  
+  if(gameState === 0){
+    playButton.display();
+    fill("black");
+    textSize(40);
+    text("1 V 1 FOOTBALL GAME", 600 , 55);
+
+    fill("yellow");
+    textSize(25);
+    text("> THE RULES OF THE GAME ARE GIVEN BELOW",100,120);
+    text("> THIS GAME CONSISTS OF TWO PLAYER",100,150);
+    text("> PLAYER TO YOUR LEFT IS YOU (I.E. THE PLAYER)",100,180);
+    text("> THE PLAYER TO YOUR RIGHT IS YOUR OPPONENT",100,210);
+    text("> YOU CAN MOVE THE PLAYERS WITH THE HELP OF THE 4 ARROW KEYS",100,240);
+    text("> THE PLAYER TO SCORE THE FIRST 5 GOALS WINS !!!!",100,270);
+  }
+
+  if(gameState === 1){
+    background(backGroundImage);
+    drawSprites();
+  }
+}
